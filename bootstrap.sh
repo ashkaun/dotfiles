86708b3f8dea01f6f039a011c894722f300de861
ashkaun <ashkaun@gmail.com>
2016-12-04 12:04:57 -0800

    Update .macos and bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,9 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
+		--exclude ".macos" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
